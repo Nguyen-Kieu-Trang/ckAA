7e49d9e90370048c18747c9efaf1c2fcd8013984
Nguyen Kieu Trang <trang.nk205174@sis.hust.edu.vn>
2023-03-11 11:35:39 +0700

    de o duoi cung

--- demo.cpp
+++ demo.cpp
@@ -4,5 +4,22 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     // đề ở đây
+    // test lai
+    do
+    {
+        cin >> call;
+        string S;
+        if (call == "#")
+            break;
+        if (call == "?number_calls_from")
+        {
+            cin >> S;
+            cout << numberCall[S] << endl;
+        }
+        else
+        {
+            break;
+        }
+    } while (call != "#");
     return 0;
 }
